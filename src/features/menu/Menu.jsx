fc039a8e63e7362bc72c1ceea726772de85c2b18
BRGOKA <bgoudjal3@gmail.com>
2026-04-29 00:08:28 +0100

    fixed the menu display

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -5,14 +5,14 @@ import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData();
   return (
-    <>
-      <h1>Menu</h1>;
-      <ul>
+    <div className="bg-yellow-100">
+      <h1 className="mb-8 flex justify-center pt-8 text-2xl font-bold">Menu</h1>
+      <ul className="grid grid-cols-3 gap-6 px-6 md:grid-cols-5">
         {menu.map((pizza) => (
           <MenuItem key={pizza.id} pizza={pizza} />
         ))}
       </ul>
-    </>
+    </div>
   );
 }
 
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -4,9 +4,13 @@ function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
+    <li className="overflow-hidden rounded-t-xl bg-amber-200 shadow-lg transition-all duration-300 hover:scale-110 hover:cursor-pointer">
+      <img
+        className="w-screen transition-all duration-300 hover:scale-110"
+        src={imageUrl}
+        alt={name}
+      />
+      <div className="p-2">
         <p>{name}</p>
         <p>{ingredients.join(", ")}</p>
         <div>
