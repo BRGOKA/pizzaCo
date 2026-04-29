016141bee29ae57a05eea82b81844b0c0b8a5830
BRGOKA <bgoudjal3@gmail.com>
2026-04-29 14:03:30 +0100

    disabled the scrollbar display

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -5,7 +5,7 @@ import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData();
   return (
-    <div className="bg-stone-200">
+    <div>
       <h1 className="mb-4 flex justify-center pt-4 text-2xl font-bold">Menu</h1>
       <ul className="divide-y divide-stone-400 pl-2">
         {menu.map((pizza) => (
