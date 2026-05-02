9212e27b02ff599be80fbac784305edca1506dde
BRGOKA <bgoudjal3@gmail.com>
2026-05-02 16:50:02 +0100

    fixed cart overview

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,14 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function CartOverview() {
+  const cart = useSelector((state) => state.cart.cart);
+  const total = cart.reduce((acc, curr) => acc + curr.totalPrice, 0);
   return (
     <div className="flex items-center justify-between bg-stone-950 p-4 sm:px-8">
       <p className="space-x-4 font-semibold uppercase text-stone-200">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{cart.length} pizzas</span>
+        <span>${total}</span>
       </p>
       <Link to="/cart" className="font-semibold uppercase text-stone-200">
         Open cart &rarr;
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [
+    {
+      pizzaId: 12,
+      name: "nappoli",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    delItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    incItemQuant(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+    },
+    decItemQuant(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+    },
+    clearCart(state, action) {
+      state.cart = [];
+    },
+  },
+});
+
+export const { addItem, delItem, incItemQuant, decItemQuant, clearCart } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
